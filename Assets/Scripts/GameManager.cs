fd264354ab8fe190565e96643a826677c95c6a8d
phoenix0111 <shashankmalhotraop@gmail.com>
2026-02-18 12:47:20 +0530

    best time mechanic implementation
    It decides best time for solving the puzzle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,29 +13,41 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI movesText;
 
+
+    [Header("Best Time UI")]
+    [SerializeField] private TextMeshProUGUI bestTimeText;
+    private const string BEST_TIME_KEY = "BestTime";
+    private float bestTime = 0f;
+    public float totalTime = 60f;
+    public float timeRemaining = 0f;
+
+
     private int matchedPairs = 0;
     private int totalPairs;
 
     private int score = 0;
     private int moves = 0;
 
-    public float totalTime = 60f;
+    
     private bool isPlaying = true;
     public int scorePerPair = 1;
 
-    private void Start()
+    void Start()
     {
+        LoadBestTime();
+        UpdateBestTimeUI();
+
         totalPairs = gridManager.GetTotalPairs();
         UpdateUI();
+        timeRemaining = totalTime;
     }
 
-    private void Update()
+    void Update()
     {
         if (!isPlaying) return;
 
-        totalTime -= Time.deltaTime;
-
-        timerText.text = "Time: " + Mathf.FloorToInt(totalTime);
+        timeRemaining -= Time.deltaTime;
+        timerText.text = "Time: " + Mathf.FloorToInt(timeRemaining);
 
     }
 
@@ -48,6 +60,9 @@ public class GameManager : MonoBehaviour
         if (matchedPairs >= totalPairs)
         {
             isPlaying = false;
+            CheckForBestTime();
+            SaveBestTime();
+            UpdateBestTimeUI();
             winPanel.SetActive(true);
         }
 
@@ -63,7 +78,7 @@ public class GameManager : MonoBehaviour
 
     private void UpdateUI()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Matches: " + score;
         movesText.text = "Moves: " + moves;
     }
 
@@ -76,6 +91,52 @@ public class GameManager : MonoBehaviour
     //    isPlaying = true;
 
     //    winPanel.SetActive(false);
-   
-   // SceneManager.LoadScene("MainMenu");
+
+    // SceneManager.LoadScene("MainMenu");
+
+    private void CheckForBestTime()
+    {
+       float newTime;
+
+        newTime = totalTime - timeRemaining;
+
+        if(bestTime>newTime) 
+        {
+            bestTime = newTime;
+        }
+        else if (bestTime == 0f)
+        {
+            bestTime = newTime;
+        }
+
+        Debug.Log("Best time " + bestTime);
+    }
+
+    private void SaveBestTime()
+    {
+
+        PlayerPrefs.SetFloat(BEST_TIME_KEY, bestTime);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadBestTime()
+    {
+        bestTime = PlayerPrefs.GetFloat(BEST_TIME_KEY, 0f);
+    }
+
+    private void UpdateBestTimeUI()
+    {
+        if (bestTime <= 0f)
+        {
+            bestTimeText.text = "Best Time: --";
+            return;
+        }
+
+
+        int seconds = Mathf.FloorToInt(bestTime);
+        
+
+        bestTimeText.text = "Best Time: " +"\n" + seconds + "sec";
+    }
+
 }
